f7676ecc1ad577d35bb28e00402b4db1a57d3deb
maxfarma <maxfarma.pages@gmail.com>
2026-06-17 19:30:13 -0300

    fix: firebase.js exporta db, CSP cloudinary

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,17 +1,17 @@
-import { initializeApp, getApps } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+import { initializeApp, getApps } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey:            "AIzaSyAygqNToiMpqkRAXpnwrJS6x1VpHl7Ogk8",
+  authDomain:        "maxfarma-feaab.firebaseapp.com",
+  projectId:         "maxfarma-feaab",
+  storageBucket:     "maxfarma-feaab.firebasestorage.app",
+  messagingSenderId: "632353368196",
+  appId:             "1:632353368196:web:7ce39e7cde191765593ad1",
 };
 
-// Inicializa Firebase solo si no se inicializó antes
+// Evita inicializar la app más de una vez (importante en Next.js)
 const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
 
-// Exportamos la base de datos de Firestore lista para usar
 export const db = getFirestore(app);
+export default app;
